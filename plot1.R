0c9c7dd4de6243e76b0605037cbad120cd1f7dc3
Jakub Rybinski <jakub.rybinski@ebc.uu.se>
2014-09-30 15:17:23 +0200

    back to original

--- plot1.R
+++ plot1.R
@@ -23,5 +23,3 @@ hist(data$Global_active_power, main="Global Active Power",
 # Saving plot #1 to file.
 dev.copy(png, file="plot1.png", height=480, width=480)
 dev.off()
-
-### Here comes the change. Buuhoo!
